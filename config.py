33d9270155cb768a5d89037e548cef20c1084ec3
hakimihk <efootballhakimi2@gmail.com>
2026-05-07 21:13:35 +0300

    Refactor config.py with hardcoded values
    Updated configuration variables to use hardcoded values and added a new webhook URL.

--- config.py
+++ config.py
@@ -3,12 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-TOKEN = os.getenv("8721581472:AAEL4QKl_AoJ2caDhp9nBJxJODa-iAhBP-o", #"YOUR_TOKEN_HERE")
-ADMIN_IDS = [8551276141]  # Replace with actual IDs
-DATABASE_PATH = "efootball.db"
-WEBHOOK_URL = os.getenv("WEBHOOK_URL") # For Katabump/VPS
-PORT = int(os.environ.get('PORT', 5000))
+TOKEN = "8721581472:AAEL4QKl_AoJ2caDhp9nBJxJODa-iAhBP-o"
+ADMIN_IDS = [8551276141]
+DATABASE_PATH = "efootball.db"  # Line-kan ayaa kuu dhimman!
 
-# Settings
-ROOM_CODE_REGEX = r'^\d{8}$'
-EXPIRE_TIME = 3600  # 1 Hour
+# Settings-ka kale
+WEBHOOK_URL = "https://example.onrender.com/"
+PORT = int(os.environ.get('PORT', 5000))
